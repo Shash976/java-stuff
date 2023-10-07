54ef25819ba3a66ad7814c49b578cfca01f0e18c
Shashwat Goel <itshashgoel@gmail.com>
2023-10-07 16:00:59 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args){
+        String[] words = {"TEN", "NINE"};
+        for (int i=0; i<words.length; i++){
+            String w = words[i];
+            System.out.println("The word is: "+w);
+        }
+    }
+}
